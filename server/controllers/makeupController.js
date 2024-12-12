c9cd66e9d0334d89fae5a12a1aa55547d6b4c4ae
Sandra Bernardo <125202261+sjbernardo@users.noreply.github.com>
2024-12-12 23:18:44 +0800

    Update makeupController.js

--- server/controllers/makeupController.js
+++ server/controllers/makeupController.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose');
 const Makeup = require('../../models/makeup');
 
-// Open the connection
+// Open the connection to MongoDB database
 mongoose.connect('mongodb://127.0.0.1:27017/makeupDB')
     .then(() => {
-        console.log('Connection Open');
+        console.log('Connection Open'); // Log success if connection is established
     })
     .catch(err => {
-        console.log('Error');
-        console.log(err);
+        console.log('Error'); // Log error if connection fails
+        console.log(err); 
     });
 
-// List of all makeup
+// List all makeup items
 exports.listMakeup = async(req, res) => {
     const makeups = await Makeup.find({});
     res.render('index', { makeups, successMessage: req.query.success });
 }
 
-// New makeup form
+// Form to add a new makeup item
 exports.newMakeup = (req, res) => {
-    res.render('new');
+    res.render('new'); 
 };
 
-// Add New makeup (values)
+// Add a new makeup item to the database
 exports.addMakeup = async(req, res) => {
     const { name, category, quantity, price, description } = req.body;
     try {
@@ -35,33 +35,28 @@ exports.addMakeup = async(req, res) => {
     }
 }
 
-// View Specific Makeup
+// View a specific makeup item by its ID
 exports.viewMakeup = async(req, res) => {
     const makeup = await Makeup.findById(req.params.id);
     res.render('show', { makeup });
 }
 
-// Edit Makeup Form
+// Form to edit an existing makeup item
 exports.editMakeup = async(req, res) => {
     const makeup = await Makeup.findById(req.params.id);
     res.render('edit', { makeup });
 }
 
-// Update Makeup
+// Update an existing makeup item in the database
 exports.updateMakeup = async(req, res) => {
     const { name, category, quantity, price, description } = req.body;
     await Makeup.findByIdAndUpdate(req.params.id, { name, category, quantity, price, description });
     res.redirect('/makeups?success=Item updated successfully!');
 }
 
-// Delete Form
-exports.delMakeupConfirmation = async(req, res) => {
-    const makeup = await Makeup.findById(req.params.id);
-    res.render('delete', { makeup });
-}
-
-// Delete Makeup
-exports.delMakeup = async(req, res) => {
-    await Makeup.findByIdAndDelete(req.params.id);
+// Delete a specific makeup item from the database
+exports.deleteMakeup = async (req, res) => {
+    const {id} = req.params;
+    await Makeup.findByIdAndDelete(id);
     res.redirect('/makeups?success=Item deleted successfully!');
 }
